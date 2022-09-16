a0beee83133362a250f1c5da4c35d42fabcdb9ea
nuelobeto <nuelobeto@gmail.com>
2022-09-16 13:00:11 +0100

    added up-down arrow to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import "../assests/css/header.css";
-import upDownArrown from "../assests/images/updown-arrow.svg";
+import UpDownArrow from "./../assests/svg/UpDownArrow";
 
 const Header = () => {
   return (
     <header>
       <div className="project-name">
         <span>Small Chops</span>
-        <img src={upDownArrown} alt="arrow" />
+        <UpDownArrow />
       </div>
     </header>
   );
