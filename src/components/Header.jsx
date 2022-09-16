f3199dd38c38ec5b8f52f0253b10452061970545
nuelobeto <nuelobeto@gmail.com>
2022-09-16 10:28:54 +0100

    added project name to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import "../assests/css/header.css";
+import upDownArrown from "../assests/images/updown-arrow.svg";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div className="project-name">
+        <span>Small Chops</span>
+        <img src={upDownArrown} alt="arrow" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
