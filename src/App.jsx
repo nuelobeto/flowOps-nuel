0c69122012d0f553c8ebdd9be12bda8abf35d1c2
nuelobeto <nuelobeto@gmail.com>
2022-09-16 15:51:08 +0100

    created sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+const SignUp = React.lazy(() => import("./auth/SignUp"));
 const Deck = React.lazy(() => import("./dashboard/deck/Deck"));
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<h1>Loading</h1>}>
+              <SignUp />
+            </Suspense>
+          }
+        />
         <Route
           path="/dashboard"
           element={
